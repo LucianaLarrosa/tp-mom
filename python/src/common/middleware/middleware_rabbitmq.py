2bed0afd7891d93e347db1d86354742fd0514512
LucianaLarrosa <luciana.larrosa@gmail.com>
2026-04-06 17:28:08 -0300

    style: format code

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,18 +1,25 @@
 import pika
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange, MessageMiddlewareDisconnectedError, MessageMiddlewareMessageError, MessageMiddlewareCloseError
+from .middleware import (
+    MessageMiddlewareQueue,
+    MessageMiddlewareExchange,
+    MessageMiddlewareDisconnectedError,
+    MessageMiddlewareMessageError,
+    MessageMiddlewareCloseError,
+)
+
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
         self.queue_name = queue_name
         self._on_message_callback = None
-        
+
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=self.queue_name)
 
     def _pika_callback(self, ch, method, properties, body):
-        #Adapta la firma del callback de pika a la interfaz del middleware.
+        # Adapta la firma del callback de pika a la interfaz del middleware.
         ack = lambda: ch.basic_ack(delivery_tag=method.delivery_tag)
         nack = lambda: ch.basic_nack(delivery_tag=method.delivery_tag)
         self._on_message_callback(body, ack, nack)
@@ -22,11 +29,14 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             self._on_message_callback = on_message_callback
             self.channel.basic_qos(prefetch_count=1)
             self.channel.basic_consume(
-                queue=self.queue_name,
-                on_message_callback=self._pika_callback
+                queue=self.queue_name, on_message_callback=self._pika_callback
             )
             self.channel.start_consuming()
-        except (pika.exceptions.AMQPConnectionError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.StreamLostError):
+        except (
+            pika.exceptions.AMQPConnectionError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.StreamLostError,
+        ):
             raise MessageMiddlewareDisconnectedError()
         except pika.exceptions.AMQPError:
             raise MessageMiddlewareMessageError()
@@ -34,20 +44,28 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     def stop_consuming(self):
         try:
             self.channel.stop_consuming()
-        except (pika.exceptions.AMQPConnectionError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.StreamLostError):
+        except (
+            pika.exceptions.AMQPConnectionError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.StreamLostError,
+        ):
             raise MessageMiddlewareDisconnectedError()
 
     def send(self, message):
         try:
             self.channel.basic_publish(
-                exchange='', 
-                routing_key=self.queue_name, 
+                exchange="",
+                routing_key=self.queue_name,
                 body=message,
                 properties=pika.BasicProperties(
                     delivery_mode=pika.DeliveryMode.Persistent
-                )
+                ),
             )
-        except (pika.exceptions.AMQPConnectionError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.StreamLostError):
+        except (
+            pika.exceptions.AMQPConnectionError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.StreamLostError,
+        ):
             raise MessageMiddlewareDisconnectedError()
         except pika.exceptions.AMQPError:
             raise MessageMiddlewareMessageError()
@@ -58,8 +76,9 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         except pika.exceptions.AMQPError:
             raise MessageMiddlewareCloseError()
 
+
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
-    
+
     def __init__(self, host, exchange_name, routing_keys):
         self.exchange_name = exchange_name
         self.routing_keys = routing_keys
@@ -67,10 +86,12 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
 
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
-        
+        self.channel.exchange_declare(
+            exchange=self.exchange_name, exchange_type="direct"
+        )
+
     def _pika_callback(self, ch, method, properties, body):
-        #Adapta la firma del callback de pika a la interfaz del middleware.
+        # Adapta la firma del callback de pika a la interfaz del middleware.
         ack = lambda: ch.basic_ack(delivery_tag=method.delivery_tag)
         nack = lambda: ch.basic_nack(delivery_tag=method.delivery_tag)
         self._on_message_callback(body, ack, nack)
@@ -78,20 +99,23 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def start_consuming(self, on_message_callback):
         try:
             self._on_message_callback = on_message_callback
-            result = self.channel.queue_declare(queue='', exclusive=True)
+            result = self.channel.queue_declare(queue="", exclusive=True)
             queue_name = result.method.queue
             for routing_key in self.routing_keys:
                 self.channel.queue_bind(
                     exchange=self.exchange_name,
                     queue=queue_name,
-                    routing_key=routing_key
+                    routing_key=routing_key,
                 )
             self.channel.basic_consume(
-                queue=queue_name,
-                on_message_callback=self._pika_callback
+                queue=queue_name, on_message_callback=self._pika_callback
             )
             self.channel.start_consuming()
-        except (pika.exceptions.AMQPConnectionError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.StreamLostError):
+        except (
+            pika.exceptions.AMQPConnectionError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.StreamLostError,
+        ):
             raise MessageMiddlewareDisconnectedError()
         except pika.exceptions.AMQPError:
             raise MessageMiddlewareMessageError()
@@ -99,7 +123,11 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def stop_consuming(self):
         try:
             self.channel.stop_consuming()
-        except (pika.exceptions.AMQPConnectionError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.StreamLostError):
+        except (
+            pika.exceptions.AMQPConnectionError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.StreamLostError,
+        ):
             raise MessageMiddlewareDisconnectedError()
 
     def send(self, message):
@@ -110,9 +138,13 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
                 body=message,
                 properties=pika.BasicProperties(
                     delivery_mode=pika.DeliveryMode.Persistent
-                )
+                ),
             )
-        except (pika.exceptions.AMQPConnectionError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.StreamLostError):
+        except (
+            pika.exceptions.AMQPConnectionError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.StreamLostError,
+        ):
             raise MessageMiddlewareDisconnectedError()
         except pika.exceptions.AMQPError:
             raise MessageMiddlewareMessageError()
